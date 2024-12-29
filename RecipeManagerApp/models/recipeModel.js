a028f2d2ee2dbb77a82e7c303ace0bfac6d10c83
Michal <rfrttfgtrtfgrr>
2024-12-29 20:00:57 +0100

    validation added

--- RecipeManagerApp/models/recipeModel.js
+++ RecipeManagerApp/models/recipeModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const RecipeSchema = new mongoose.Schema({
-    nazwa : {type: String  ,maxlength: 30,required: true},
-    ingredients : {type: [String] , maxlength: 20,required: true},
-    instruction : {type : String , maxlength: 100,required: true},
-    rate : {type: Number , max : 5,required: true},
+    nazwa : {type: String  ,minLength:5 ,maxlength: 30,required: true},
+    ingredients : {type: [String] ,minLength:2, maxlength: 20,required: true},
+    instruction : {type : String ,minLength : 10, maxlength: 100,required: true},
+    rate : {type: Number ,min:0, max : 5,required: true},
 })
 
 module.exports = mongoose.model("Recipe" , RecipeSchema) //nazwa , schemat
