c527573333b99f919432f6d39b8d0c3ccc9f3b8a
Michal <rfrttfgtrtfgrr>
2024-12-29 19:42:08 +0100

    CRUD completed

--- RecipeManagerApp/models/recipeModel.js
+++ RecipeManagerApp/models/recipeModel.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const RecipeSchema = mongoose.Schema({
-    nazwa : {type: String , maxlength: 30},
-    ingredients : {type: [String] , maxlength: 20},
-    instruction : {type : String , maxlength: 100},
-    rate : {type: Number , max : 5},
+const RecipeSchema = new mongoose.Schema({
+    nazwa : {type: String  ,maxlength: 30,required: true},
+    ingredients : {type: [String] , maxlength: 20,required: true},
+    instruction : {type : String , maxlength: 100,required: true},
+    rate : {type: Number , max : 5,required: true},
 })
 
 module.exports = mongoose.model("Recipe" , RecipeSchema) //nazwa , schemat
--- RecipeManagerApp/routes/recipe.js
+++ RecipeManagerApp/routes/recipe.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 var Recipe = require('../models/recipeModel');
 const express = require("express");
 var router = express.Router();
@@ -12,15 +11,35 @@ router.get('/', async (req, res) => {
         res.status(400).send({error: e});
     }
 })
-router.post('/abc' , async (req, res) => {
+router.post('/recipe' , async (req, res) => {
     try {
         const {nazwa, ingredients, instruction, rate} = req.body; //pobranie danych z body (destrukturyzacja)
         const newRecipe = new Recipe({
             nazwa, ingredients, instruction, rate
         })
         const saveRecipe = await newRecipe.save();
+        res.status(201).json(saveRecipe);
     }
     catch (e) {
+        res.status(400).send({error: e});
+    }
+})
+router.put('/recipe/:id' , async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {nazwa, ingredients, instruction, rate} = req.body;
+        const newRecipe = await Recipe.findByIdAndUpdate(id ,{nazwa , ingredients , instruction , rate} , {new : true , runValidators : true})
+        res.status(201).json(newRecipe);
+    }catch (e) {
+        res.status(400).send({error: e});
+    }
+})
+router.delete('/recipe/:id' , async (req, res) => {
+    try{
+        const {id} = req.params;
+        const recipeToDelete = await Recipe.findByIdAndDelete(id)
+        res.status(201).json(recipeToDelete);
+    }catch (e) {
         res.status(400).send({error: e});
     }
 })
